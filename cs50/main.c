5b49558edeeb343737653ef830b1d534bf59159a
Angelo Villegas <gelo.web@gmail.com>
2015-12-29 15:43:31 +0800

    Initial Commit

---
+++ cs50/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cs50
+//
+//  Created by Angelo Villegas on 29/12/15.
+//  Copyright © 2015 Angelo Villegas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
